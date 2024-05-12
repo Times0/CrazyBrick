9ce784be5a6604d5d6d09a62e3e4ba6c3bce86ad
Times0 <45049767+Times0@users.noreply.github.com>
2024-05-12 05:32:42 +0200

    improve ball collision with polygon

--- src/ball.cpp
+++ src/ball.cpp
@@ -25,32 +25,44 @@ void ball::draw(SDL_Renderer *renderer) const {
 
 bool ball::handleSolidCollision(const Polygon &polygon_points) {
     // This function is dependent on the order of the points in the polygon
+    float min_ditance = 1000000;
+    std::pair<Vector2, Vector2> mypair = {{0, 0},
+                                          {0, 0}};
     for (size_t i = 0; i < polygon_points.size(); ++i) {
         size_t next_index = (i + 1) % polygon_points.size();
         const Vector2 &p1 = polygon_points[i];
         const Vector2 &p2 = polygon_points[next_index];
 
         float distance = pointLineDistance({center.x, center.y}, {p1.x, p1.y}, {p2.x, p2.y});
-        if (distance < radius) {
-            float dx = p2.x - p1.x;
-            float dy = p2.y - p1.y;
-            float normal_x = -dy;
-            float normal_y = dx;
 
-            float length = std::sqrt(normal_x * normal_x + normal_y * normal_y);
-            normal_x /= length;
-            normal_y /= length;
-
-            // Set the ball's position just outside
-            center = {center.x + normal_x * (radius - distance),
-                      center.y + normal_y * (radius - distance)};
-
-            float dot_product = velocity.x * normal_x + velocity.y * normal_y;
-
-            velocity = {velocity.x - 2 * dot_product * normal_x,
-                        velocity.y - 2 * dot_product * normal_y};
-            return true;
+        if (distance < min_ditance) {
+            min_ditance = distance;
+            mypair = {p1, p2};
         }
+    }
+    if (min_ditance < radius) {
+        const Vector2 &p1 = mypair.first;
+        const Vector2 &p2 = mypair.second;
+
+        float dx = p2.x - p1.x;
+        float dy = p2.y - p1.y;
+        float normal_x = -dy;
+        float normal_y = dx;
+
+        float length = std::sqrt(normal_x * normal_x + normal_y * normal_y);
+        normal_x /= length;
+        normal_y /= length;
+
+        // Set the ball's position just outside
+        center = {center.x + normal_x * (radius - min_ditance),
+                  center.y + normal_y * (radius - min_ditance)};
+
+        float dot_product = velocity.x * normal_x + velocity.y * normal_y;
+
+        velocity = {velocity.x - 2 * dot_product * normal_x,
+                    velocity.y - 2 * dot_product * normal_y};
+        return true;
+
     }
     return false;
 
